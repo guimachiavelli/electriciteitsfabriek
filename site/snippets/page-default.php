1cabb827f80ffa16425961cb699c4df5e0479d2a
gui machiavelli <hey@guimachiavelli.com>
2016-09-06 18:29:30 +0200

    add default/about section styling

--- site/snippets/page-default.php
+++ site/snippets/page-default.php
@@ -1,4 +1,11 @@
-<div class="">
-    <h1><?php echo $content->title()->html() ?></h1>
-    <?php echo $content->text()->kirbytext() ?>
-</div>
+<section class="site-section site-section--default">
+    <header class="site-section__header">
+        <h2 class="site-section__title">
+            <?php echo $content->title()->html(); ?>
+        </h2>
+    </header>
+
+    <div class="site-section__default-content">
+        <?php echo $content->text()->kirbytext(); ?>
+    </div>
+</section>
